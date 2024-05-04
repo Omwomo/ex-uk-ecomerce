8daab30cc75bc0333e8d09cab38b2212e9d093c3
omwomo <eugeneoduor79@gmail.com>
2024-05-04 12:34:32 +0300

    Created actions for my products controller

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,51 @@
 class Api::V1::ProductsController < ApplicationController
-end
+    before_action :authenticate_user!, except: [:index, :show]
+    load_and_authorize_resource
+  
+    # GET /products
+    def index
+      @products = Product.all
+      render json: @products
+    end
+  
+    # GET /products/:id
+    def show
+      @product = Product.find(params[:id])
+      render json: @product
+    end
+  
+    # POST /products
+    def create
+      @product = Product.new(product_params)
+      if @product.save
+        render json: @product, status: :created
+      else
+        render json: @product.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /products/:id
+    def update
+      @product = Product.find(params[:id])
+      if @product.update(product_params)
+        render json: @product
+      else
+        render json: @product.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /products/:id
+    def destroy
+      @product = Product.find(params[:id])
+      @product.destroy
+      head :no_content
+    end
+  
+    private
+  
+    # Only allow a trusted parameter "white list" through.
+    def product_params
+      params.require(:product).permit(:name, :image, :description, :price, :category_id)
+    end
+  end
+  
