060d49e3ccac0483a90089c8c43a4f7f7712dab3
omwomo <eugeneoduor79@gmail.com>
2024-05-06 09:36:02 +0300

    Created actions for my orderItems controller

---
+++ app/controllers/api/v1/order_items_controller.rb
@@ -0,0 +1,38 @@
+class Api::V1::OrderItemsController < ApplicationController
+    # before_action :authenticate_user!
+    load_and_authorize_resource
+  
+    # POST /order_items
+    def create
+      @order_item = OrderItem.new(order_item_params)
+      if @order_item.save
+        render json: @order_item, status: :created
+      else
+        render json: @order_item.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # PATCH/PUT /order_items/:id
+    def update
+      @order_item = OrderItem.find(params[:id])
+      if @order_item.update(order_item_params)
+        render json: @order_item
+      else
+        render json: @order_item.errors, status: :unprocessable_entity
+      end
+    end
+  
+    # DELETE /order_items/:id
+    def destroy
+      @order_item = OrderItem.find(params[:id])
+      @order_item.destroy
+      head :no_content
+    end
+  
+    private
+  
+    # Only allow a trusted parameter "white list" through.
+    def order_item_params
+      params.require(:order_item).permit(:order_id, :product_id, :quantity, :subtotal_price)
+    end
+  end  
