149195beb45507547340c5be3266e290b85657e3
omwomo <eugeneoduor79@gmail.com>
2024-05-08 09:15:02 +0300

    created actions for fetching products and categories

--- app/javascript/redux/actions.js
+++ app/javascript/redux/actions.js
@@ -1,10 +1,38 @@
-import { setProduct } from './slices/productSlice';
+import { setCategories, setProducts, setProduct } from './slices/slices';
+// import { fetch } from 'whatwg-fetch';
 
-export const fetchProducts = () => {
-  return (dispatch) => {
-    fetch('/api/v1/products')
-      .then((response) => response.json())
-      .then((data) => dispatch(setProduct(data)))
-      .catch((error) => console.error('Error fetching greeting:', error));
+export const fetchCategories = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch('/api/v1/categories');
+      const data = await response.json();
+      dispatch(setCategories(data));
+    } catch (error) {
+      console.error('Error fetching categories:', error);
+    }
+  };
+};
+
+export const fetchProductsByCategory = (categoryId) => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(`/api/v1/categories/${categoryId}/products`);
+      const data = await response.json();
+      dispatch(setProducts(data));
+    } catch (error) {
+      console.error('Error fetching products by category:', error);
+    }
+  };
+};
+
+export const fetchProduct = (productId) => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(`/api/v1/products/${productId}`);
+      const data = await response.json();
+      dispatch(setProduct(data));
+    } catch (error) {
+      console.error('Error fetching product details:', error);
+    }
   };
 };
