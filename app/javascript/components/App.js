2c590bfb92eec35307eb8141f71eace554e51f12
omwomo <eugeneoduor79@gmail.com>
2024-05-09 16:27:56 +0300

    Updated routing

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Header';
 import Category from './Category';
 import ProductList from './ProductList';
 import ProductDetail from './ProductDetail';
+import Cart from './Cart';
 
 const App = () => {
   return (
     <Router>
+      <Header />
       <Routes>
-        <Route path='/' element={<Category />} />
-        <Route path='/category/:categoryId' element={<ProductList />} />
-        <Route path='/product/:productId' element={<ProductDetail />} />
+        <Route path="/" element={<Category />} />
+        <Route path="/category/:categoryId" element={<ProductList />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
     </Router>
   );
