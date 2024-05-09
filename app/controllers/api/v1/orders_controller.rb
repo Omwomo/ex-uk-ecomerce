4765b37fbcb60a6f60938d74f61ef5b0ba178c8e
omwomo <eugeneoduor79@gmail.com>
2024-05-09 03:49:20 +0300

    Refactored my order controller to allow guest user to create cart

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -1,29 +1,22 @@
 class Api::V1::OrdersController < ApplicationController
     # before_action :authenticate_user!, except: [:create]
-    load_and_authorize_resource
+    # load_and_authorize_resource
   
     # GET /orders
     def index
-      @orders = current_user.orders if user_signed_in?
+      @orders = current_order
       render json: @orders
     end
   
     # GET /orders/:id
     def show
-      @order = current_user.orders.find(params[:id]) if user_signed_in?
+      @order = current_cart
       render json: @order
     end
   
     # POST /orders
     def create
-      if user_signed_in?
-        @order = current_user.orders.build(order_params)
-      else
-        # Create a guest user or anonymous user
-        guest_user = User.create_guest_user
-        @order = guest_user.orders.build(order_params)
-      end
-  
+      @order = current_order
       if @order.save
         render json: @order, status: :created
       else
@@ -33,7 +26,7 @@ class Api::V1::OrdersController < ApplicationController
   
     # PATCH/PUT /orders/:id
     def update
-      @order = current_user.orders.find(params[:id]) if user_signed_in?
+      @order = current_order
       if @order.update(order_params)
         render json: @order
       else
@@ -43,16 +36,28 @@ class Api::V1::OrdersController < ApplicationController
   
     # DELETE /orders/:id
     def destroy
-      @order = current_user.orders.find(params[:id]) if user_signed_in?
+      @order = current_order
       @order.destroy
       head :no_content
     end
   
     private
+
+    def current_order
+      if user_signed_in?
+        current_user.order || current_user.build_order
+      else
+        guest_order || User.create_guest_user.build_order
+      end
+    end
+
+    def guest_order
+      Order.find_by(id: cookies.signed[:order_id])
+    end
   
     # Only allow a trusted parameter "white list" through.
     def order_params
       params.require(:order).permit(:status, :total_price)
     end
-  end
+end
   
