7ce977207ef5f201b287562662f2bc9b97376d84
omwomo <eugeneoduor79@gmail.com>
2024-05-09 16:22:48 +0300

    Created guest user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,25 @@ class User < ApplicationRecord
   
   has_many :reviews
   has_many :orders
+
+  def build_order
+    orders.build
+  end
+
+  def self.create_guest_user
+    # Try to find an existing guest user
+    user = User.find_by(email: 'guest@example.com')
+
+    # If a guest user is found, return it
+    return user if user
+
+    # If no guest user is found, create a new one
+    user = User.create(email: 'guest@example.com', firstname: 'Guest', lastname: 'User', password: 'guestpassword', role: 'guest')
+
+    # Create an order for the guest user
+    user.orders.create(status: nil, total_price: nil)
+
+    # Return the newly created guest user
+    user
+  end
 end
