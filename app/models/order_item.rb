1e57f2b10146d42ecbbf2897ef690ac12f186503
omwomo <eugeneoduor79@gmail.com>
2024-05-17 11:10:58 +0300

    Create mehod that calculates subtotal price

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,10 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :product
+
+  before_save :set_subtotal_price
+
+  def set_subtotal_price
+    self.subtotal_price = quantity * product.price
+  end
 end
