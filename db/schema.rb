67885596a96717bd07210ed6adc498b5bbba14f2
Omwomo <eugeneoduor79@gmail.com>
2024-05-17 10:18:07 +0300

    Updated my migration files to remove dublicate migrations

--- db/schema.rb
+++ db/schema.rb
@@ -80,6 +80,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_14_074448) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "role"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -87,7 +88,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_14_074448) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "role"
     t.string "firstname"
     t.string "lastname"
     t.string "unique_identifier"
