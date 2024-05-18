ae483dff2fd3658dc3850c50ef10f8b7f82cd5f1
omwomo <eugeneoduor79@gmail.com>
2024-05-18 09:41:06 +0300

    update total price before save

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,10 @@
 class Order < ApplicationRecord
   belongs_to :user
   has_many :order_items
+
+  before_save :update_total_price
+
+  def update_total_price
+    self.total_price = order_items.sum(:subtotal_price)
+  end
 end
