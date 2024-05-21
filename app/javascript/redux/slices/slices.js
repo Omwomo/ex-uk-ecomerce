a653369b06f1359988bf3fb498c8d2062a786dec
omwomo <eugeneoduor79@gmail.com>
2024-05-21 09:35:09 +0300

    Set current user reducer

--- app/javascript/redux/slices/slices.js
+++ app/javascript/redux/slices/slices.js
@@ -6,6 +6,7 @@ const initialState = {
   products: [],
   cartItems: [],
   product: {},
+  user: null,
 };
 
 const appSlice = createSlice({
@@ -36,11 +37,14 @@ const appSlice = createSlice({
     removeCartItemFromState(state, action) {
       state.cartItems = state.cartItems.filter(item => item.id !== action.payload);
     },
+    setCurrentUser(state, action) {
+      state.user = action.payload;
+    },
     setLoading(state, action) {
       state.loading = action.payload;
     },
   },
 });
 
-export const { setCategories, setProducts, setProduct, setLoading, addCartItem, setCartItems, updateCartItemInState, removeCartItemFromState } = appSlice.actions;
+export const { setCategories, setProducts, setProduct, setLoading, addCartItem, setCartItems, updateCartItemInState, removeCartItemFromState, setCurrentUser } = appSlice.actions;
 export default appSlice.reducer;
