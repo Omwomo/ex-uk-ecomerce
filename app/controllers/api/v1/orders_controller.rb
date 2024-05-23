53fde0ec5aeb22fc3b3b22c57f3975b9f38003f8
omwomo <eugeneoduor79@gmail.com>
2024-05-23 13:06:59 +0300

    Update current order method to tranfer the order from guest to current user when signed in

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -47,21 +47,32 @@ class Api::V1::OrdersController < ApplicationController
 
   def current_order
     if user_signed_in?
-      current_user.orders.first_or_create(status: 'new')
+      order = current_user.orders.find_or_create_by(status: 'new')
+      transfer_guest_order_to_user(order) if cookies.signed[:guest_order_id].present?
+      order
     else
       guest_order || create_guest_order
     end
   end
-  
+
   def guest_order
-    Order.find_by(id: cookies.signed[:order_id])
+    Order.find_by(id: cookies.signed[:guest_order_id])
   end
-  
+
   def create_guest_order
     guest_user = User.create_guest_user
     order = guest_user.orders.create(status: 'new')
-    cookies.signed[:order_id] = { value: order.id, expires: 1.year.from_now } # Persistent for 1 year
+    cookies.signed[:guest_order_id] = { value: order.id, expires: 1.year.from_now }
     order
+  end
+
+  def transfer_guest_order_to_user(user_order)
+    guest_order = Order.find_by(id: cookies.signed[:guest_order_id])
+    if guest_order
+      guest_order.order_items.update_all(order_id: user_order.id)
+      guest_order.destroy
+      cookies.delete(:guest_order_id)
+    end
   end
 
   # Only allow a trusted parameter "white list" through.
