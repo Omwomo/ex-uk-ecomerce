123815c878a26a22834c1b1e7a8acd690c28d21b
omwomo <eugeneoduor79@gmail.com>
2024-05-23 13:19:50 +0300

    Add signOut reducer

--- app/javascript/redux/slices/slices.js
+++ app/javascript/redux/slices/slices.js
@@ -40,11 +40,16 @@ const appSlice = createSlice({
     setCurrentUser(state, action) {
       state.user = action.payload;
     },
+    signOut(state) {
+      state.user = null;
+      state.cartItems = []; // Clear cart items on sign out
+      localStorage.removeItem('token');
+    },
     setLoading(state, action) {
       state.loading = action.payload;
     },
   },
 });
 
-export const { setCategories, setProducts, setProduct, setLoading, addCartItem, setCartItems, updateCartItemInState, removeCartItemFromState, setCurrentUser } = appSlice.actions;
+export const { setCategories, setProducts, setProduct, setLoading, addCartItem, setCartItems, updateCartItemInState, removeCartItemFromState, setCurrentUser, signOut } = appSlice.actions;
 export default appSlice.reducer;
