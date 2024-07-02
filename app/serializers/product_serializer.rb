407ef25089a69a584c36cffd284bf24e01923c97
omwomo <eugeneoduor79@gmail.com>
2024-07-02 11:19:37 +0300

    Include inventory in the product serializer.

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,7 +1,7 @@
 class ProductSerializer < ActiveModel::Serializer
   include Rails.application.routes.url_helpers
 
-  attributes :id, :name, :description, :price, :category_id, :image_url
+  attributes :id, :name, :description, :price, :category_id, :inventory, :image_url
 
   def image_url
     object.image.attached? ? rails_blob_url(object.image, host: 'localhost', port: 3000) : nil
