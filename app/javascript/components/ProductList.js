2d4cccf5f4195d4acf755911f70ac2cc10912eac
omwomo <eugeneoduor79@gmail.com>
2024-07-02 11:19:11 +0300

    Display remaining number of products in the product list.

--- app/javascript/components/ProductList.js
+++ app/javascript/components/ProductList.js
@@ -28,6 +28,7 @@ const ProductList = () => {
                 <div>{product.name}</div>
                 <div>{product.description}</div>
                 <div>{product.price}</div>
+                <div>{product.inventory}</div>
               </div>
             </Link>
           </li>
