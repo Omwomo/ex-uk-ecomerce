4a1d326b2cd9a4b7d2af7d6ca2a02791934d31ba
omwomo <eugeneoduor79@gmail.com>
2024-07-08 22:51:29 +0300

    Validate quantity of rder item to be greater than or equal to one.

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -4,6 +4,8 @@ class OrderItem < ApplicationRecord
 
   before_save :set_subtotal_price
 
+  validates :quantity, numericality: { greater_than_or_equal_to: 1 }
+
   def set_subtotal_price
     self.subtotal_price = quantity * product.price
   end
